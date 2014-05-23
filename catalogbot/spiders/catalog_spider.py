072df28bc2b01b9e1923de04360a0753092f14fb
Stefan Eng <stefaneng13@gmail.com>
2014-05-22 20:46:40 -0700

    Parses title and body for a course

--- catalogbot/spiders/catalog_spider.py
+++ catalogbot/spiders/catalog_spider.py
@@ -2,7 +2,7 @@ from scrapy.spider import Spider
 from scrapy.http import Request
 from scrapy.selector import Selector
 from catalogbot.items import CourseItem
-
+import re
 
 class CatalogSpider(Spider):
     name = "catalog"
@@ -13,6 +13,16 @@ class CatalogSpider(Spider):
 
     def parse_course(self, response):
         sel = Selector(response)
+
+        for course_title_sel in sel.xpath('//div[@id="courses"]/h4'):
+            course = CourseItem()
+            # Get the title line from the <h4>'s
+            title = course_title_sel.xpath('text()').extract()[0]
+
+            # Get the course body
+            body = course_title_sel.xpath('following-sibling::p[2]/text()').extract()
+
+            print title, body
 
     def parse(self, response):
         sel = Selector(response)
