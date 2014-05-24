a5f0afb863b7e4261332e65ef2a3c8850f42362c
Stefan Eng <stefaneng13@gmail.com>
2014-05-24 12:08:35 -0700

    Refractored unit tests

--- tests/test_parsetools.py
+++ tests/test_parsetools.py
@@ -1,19 +1,19 @@
 from catalogbot.parsetools import courseparser
 from catalogbot.items import CourseItem
 
-def test_parse_prereqs_em():
-    """Test the parsing of prerequisites from <em> tag"""
+def test_parse_prereqs():
+    """Test parsing of prerequisites 'Prerequisites: COMP 256/L, 333.'"""
 
     courseitem = CourseItem()
     courseitem['prereqs'] = ['COMP 256/L', 'COMP 333']
-    em_tag = "Prerequisites: COMP 256/L, 333."
+    em_tag = 'Prerequisites: COMP 256/L, 333.'
     out_courseitem = courseparser.parse_em(CourseItem(), em_tag)
 
-    print courseitem, out_courseitem
     assert courseitem == out_courseitem
 
 def test_parse_title_empty():
     """Test parsing an empty title"""
+
     courseitem = CourseItem()
     coursetitle = ''
     out_courseitem = courseparser.parse_title(CourseItem(), coursetitle)
@@ -22,6 +22,8 @@ def test_parse_title_empty():
 
 
 def test_parse_title():
+    """Parse a class title for 'COMP 310. Automata, Languages and Computation (3)'"""
+
     courseItem = CourseItem()
     courseItem['classname'] = 'COMP 310'
     courseItem['department'] = 'COMP'
@@ -31,4 +33,5 @@ def test_parse_title():
 
     coursetitle = 'COMP 310. Automata, Languages and Computation (3)'
     out_courseItem = courseparser.parse_title(CourseItem(), coursetitle)
+
     assert courseItem == out_courseItem
