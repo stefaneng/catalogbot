950b89404bca9600d0fd43cd7ddb0db4d14ca586
Stefan Eng <stefaneng13@gmail.com>
2014-05-23 21:10:43 -0700

    Working coverage with nose testing
    Should generated a report in coveralls

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='catalogbot',
     version='1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests")),
     entry_points={'scrapy': ['settings = catalogbot.settings']},
+    test_suite = 'nose.collector',
 )
