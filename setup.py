0fe93ef6a81433578601d3728acecd646e4c8582
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 11:23:33 -0700

    Moved dependencies back to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,13 @@ setup(
     version='1.0',
     packages=find_packages(exclude=("tests")),
     entry_points={'scrapy': ['settings = catalogbot.settings']},
+    dependency_links = [
+        'https://github.com/sprij/scrapy-rethinkdb/tarball/master#egg=scrapy-rethinkdb'
+    ],
+    install_requires = [
+        nose,
+        scrapy==0.22.2,
+        scrapy-rethinkdb
+    ],
     test_suite = 'nose.collector',
 )
