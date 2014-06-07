5d66e664ddfe5b56296eb6731646836a58d861e5
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 11:20:22 -0700

    Removed install_requires

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     version='1.0',
     packages=find_packages(exclude=("tests")),
     entry_points={'scrapy': ['settings = catalogbot.settings']},
-    install_requires = [r.strip('\n') for r in open('requirements.txt')],
     test_suite = 'nose.collector',
 )
