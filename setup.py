1f0ca5d385f6e4c32a964f269acb47e79ec52d94
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 11:28:57 -0700

    Added a version number for pip

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     packages=find_packages(exclude=("tests")),
     entry_points={'scrapy': ['settings = catalogbot.settings']},
     dependency_links = [
-        'https://github.com/sprij/scrapy-rethinkdb/tarball/master#egg=scrapy-rethinkdb'
+        'https://github.com/sprij/scrapy-rethinkdb/tarball/master#egg=scrapy-rethinkdb-1.0.0'
     ],
     install_requires = [
         'nose',
         'scrapy==0.22.2',
-        'scrapy-rethinkdb'
+        'scrapy-rethinkdb==1.0.0'
     ],
     test_suite = 'nose.collector',
 )
