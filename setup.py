639912c92b8ed3d17541956eda0b4535641e53f1
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 11:18:31 -0700

    Moved dep to requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     packages=find_packages(exclude=("tests")),
     entry_points={'scrapy': ['settings = catalogbot.settings']},
     install_requires = [r.strip('\n') for r in open('requirements.txt')],
-    dependency_links=['https://github.com/sprij/scrapy-rethinkdb/tarball/master#egg=package-1.0'],
     test_suite = 'nose.collector',
 )
