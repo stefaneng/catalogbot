d85903ecdf42e6607fd2de89cd4a217293114f8a
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 12:41:02 -0700

    Fixed requirements stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 setup(
     name='catalogbot',
     version='1.0',
     packages=find_packages(exclude=("tests")),
     entry_points={'scrapy': ['settings = catalogbot.settings']},
-    install_requires = [
-        str(install_req.req)
-        for install_req in parse_requirements('requirements.txt')
-    ],
     test_suite = 'nose.collector',
 )
