9af4ce8b6f65beac92081aab436c0a386aa088e5
Stefan Eng <stefaneng13@gmail.com>
2014-06-07 11:04:24 -0700

    Added dependencies to scrapy-rethinkdb
    Now inserts into rethinkdb

--- catalogbot/settings.py
+++ catalogbot/settings.py
@@ -1,17 +1,28 @@
 # Scrapy settings for catalogbot project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
 
 BOT_NAME = 'catalogbot'
 
 SPIDER_MODULES = ['catalogbot.spiders']
 NEWSPIDER_MODULE = 'catalogbot.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
+ITEM_PIPELINES = [
+  'scrapy_rethinkdb.RethinkDBPipeline',
+]
+
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'
 
 DOWNLOAD_DELAY = 5
+
+#EXTENSIONS = {
+#    'catalogbot.extensions.storestats.storestats': 500
+#}
+
+RETHINKDB_TABLE = 'catalog'
+
+RETHINKDB_CONNECTION = {
+    'db': 'catalog'
+}
+
+RETHINKDB_INSERT_OPTIONS = {
+    'upsert': True
+}
